201e38d79b875f3c0f28d0e76fe2fc7b8f002e91
aozguve1 <aozguve1@binghamton.edu>
2024-12-24 00:20:44 +0300

    "Mesajınız"

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
 import sys
 import os
 
-# Modül yolu ekleme
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from flask import Flask
 from routes.users import users
 
-app = Flask(__n
+app = Flask(__name__)  
+
+app.register_blueprint(users)
+
+@app.route("/")
+def home():
+    return "FinanceTracker API is running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
